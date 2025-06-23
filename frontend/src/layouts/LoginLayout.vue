e4e7798eafe29f4f802eb95214e15e68753746b1
Sardor <sardorbekmatniyazov@gmail.com>
2025-06-23 19:05:26 +0300

    login page

--- frontend/src/layouts/LoginLayout.vue
+++ frontend/src/layouts/LoginLayout.vue
@@ -1,98 +1,70 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 import axiosIns from "@/plugins/axios.js";
-import {URIS} from "@/constants/UriConstants.js";
-import {showMessage} from "@/util/utils.js";
-
+import { URIS } from "@/constants/UriConstants.js";
+import { showMessage } from "@/util/utils.js";
 import useUserStore from "@/store/UserStore";
 import router from "@/router/index.js";
-import {useAuthStore} from "@/store/UseAuthStore.js";
+import { useAuthStore } from "@/store/UseAuthStore.js";
 import UInput from "@/components/base/UInput.vue";
-const userStore = useUserStore();
 
+const userStore = useUserStore();
 const username = ref("username");
 const password = ref("password");
 
 function onLogin() {
-  axiosIns.post(URIS.LOGIN, {login: username.value, password: password.value})
+  axiosIns.post(URIS.LOGIN, { login: username.value, password: password.value })
       .then(res => {
         if (res.data.status === 200) {
           userStore.token = res.data.data;
-          const authStore = useAuthStore()
+          const authStore = useAuthStore();
           authStore.login(res.data.data);
           router.push("/company-layout");
         } else {
-          showMessage("Please check your login or password")
+          showMessage("Please check your login or password");
         }
-      }).catch(e => {
-    showMessage(e)
-  })
+      })
+      .catch(e => {
+        showMessage(e);
+      });
 }
 </script>
 
 <template>
-  <div class="auth-page">
-    <div class="container-fluid p-0">
-      <div class="row g-0">
-        <div class="col-xxl-3 col-lg-4 col-md-5">
-          <div class="auth-full-page-content d-flex p-sm-5 p-4">
-            <div class="w-100">
-              <div class="d-flex flex-column h-100">
-                <div class="mb-4 mb-md-5 text-center">
-                  <router-link to="/" class="d-block auth-logo">
-                    <img src="../assets/icons/em_logo.png" alt="" size="15"> <br><p class="h1 mt-3" >EFFICIENT MANAGEMENT FLEET</p>
-                  </router-link>
-                </div>
-                <div class="auth-content my-auto">
-                  <div class="text-center">
-                    <h5 class="mb-0">Welcome to EM Fleet !</h5>
-                    <p class="text-muted mt-2">Login to continue using EM Fleet..</p>
-                  </div>
-                  <form class="mt-4 pt-2">
-                    <div class="mb-3">
-                      <UInput v-model="username" placeholder="Введите имя пользователя" label="Username"/>
-                    </div>
-                    <div class="mb-3">
-                      <UInput v-model="password" placeholder="Введите пароль" label="Password" type="password"/>
-                    </div>
-                    <div class="mb-3">
-                      <button class="btn btn-primary w-100 waves-effect waves-light" type="button" @click="e => {onLogin(); e.stopPropagation()}">Login</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-xxl-9 col-lg-8 col-md-7">
-          <div class="auth-bg pt-md-5 p-4 d-flex">
-            <div class="bg-overlay bg-primary"></div>
-            <div class="row justify-content-center align-items-center">
-              <div class="col-xl-7">
-                <div class="p-0 p-sm-4 px-xl-0">
-                  <div class="carousel slide">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <div class="test-contain text-white">
-                          <i class="bx bxs-quote-alt-left text-success display-6"></i>
-
-                          <h4 class="mt-4 fw-medium lh-base text-white">“EFFICIENT MANAGEMENT FLEET – is a modern tool for task management and work organization.
-                            It allows users to plan daily tasks, set priorities, and track their completion.”
-                          </h4>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="d-flex justify-content-center align-items-center min-vh-100 bg-light rounded-4">
+    <div class="p-4 shadow rounded bg-white" style="width: 100%; max-width: 420px">
+      <div class="text-center mb-4">
+        <img src="../assets/icons/em_logo.png" alt="EM Fleet Logo" style="height: 80px;" />
+        <h2 class="mt-3">EFFICIENT MANAGEMENT FLEET</h2>
       </div>
+      <div class="text-center mb-4">
+        <h5>Welcome to EM Fleet!</h5>
+        <p class="text-muted">Login to continue using EM Fleet..</p>
+      </div>
+      <form>
+        <div class="mb-3">
+          <UInput v-model="username" placeholder="Введите имя пользователя" label="Username" />
+        </div>
+        <div class="mb-3">
+          <UInput v-model="password" placeholder="Введите пароль" label="Password" type="password" />
+        </div>
+        <div class="mb-3">
+          <button
+              class="btn btn-primary w-100"
+              type="button"
+              @click="e => { onLogin(); e.stopPropagation(); }"
+          >
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+body {
+  margin: 0;
+  background-color: #4e749d;
+}
 </style>
