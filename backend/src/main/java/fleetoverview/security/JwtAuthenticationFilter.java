dce3f2adc7685b18eb84de3df42033f450c3535d
KoBill <1810092kk@gmail.com>
2026-08-20 20:27:42 +0500

    changes - unused imports removed

--- backend/src/main/java/fleetoverview/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/fleetoverview/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package fleetoverview.security;
 
-import fleetoverview.service.impl.UserServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,12 +7,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
